2f7a6223326eed9dbff7730559cd6df59ce37216
cde-test-user <cde.test.user@gmail.com>
2018-02-21 13:46:18 +0200

    added tests

--- src/test/java/TestStringsShouldPass.java
+++ src/test/java/TestStringsShouldPass.java
@@ -8,7 +8,27 @@ import static org.junit.Assert.assertEquals;
 public class TestStringsShouldPass {
 
     @Test
-    public void testShouldPass() {
+    public void testShouldPass1() {
+        assertEquals("test", "test");
+    }
+    
+    @Test
+    public void testShouldPass2() {
+        assertEquals("test", "test");
+    }
+    
+    @Test
+    public void testShouldPass3() {
+        assertEquals("test", "test");
+    }
+    
+    @Test
+    public void testShouldPass4() {
+        assertEquals("test", "test");
+    }
+    
+    @Test
+    public void testShouldPass5() {
         assertEquals("test", "test");
     }
 
