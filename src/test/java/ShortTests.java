0e36da0a800f98a879e042c087f444aacac43183
kovan04 <andrey.kovalev@ca.com>
2018-05-31 18:34:21 +0300

    All tests successful, printing out two environment variables: MY_VAR1, MY_VAR2.

--- src/test/java/ShortTests.java
+++ src/test/java/ShortTests.java
@@ -5,11 +5,11 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by menyo01 on 01/01/2018.
  */
-public class LongTests {
+public class ShortTests {
 
     @Test
-    public void longTest() throws InterruptedException {
-        for (int i=1200; i>0; i--) {
+    public void shortTest() throws InterruptedException {
+        for (int i=5; i>0; i--) {
             System.out.println(i);
             Thread.sleep(1000L);
         }
